6409a4fbc6c41c02c8327b46a56b6e855756069d
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2023-04-12 22:17:58 +1200

    Added web using nextjs

---
+++ src/web/relay.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...
+  // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
+  src: "./src",
+  language: "typescript",
+  schema: "../../api-definitions/graphql/connectilly/schema.graphql",
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+}
+
